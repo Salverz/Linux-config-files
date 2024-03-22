dd75d80120be5b577fde72a9ebc9c04d8589a5c8
Salverz <davidorona2@gmail.com>
2024-03-21 23:01:46 -0700

    updated nvimtree keybinds

--- nvim/lua/david/plugins/nvim-tree.lua
+++ nvim/lua/david/plugins/nvim-tree.lua
@@ -5,7 +5,7 @@ return {
     },
     config = function()
         local nvimtree = require("nvim-tree")
-	
+
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 0
 		vim.g.loaded_netrePlugin = 0
@@ -16,7 +16,7 @@ return {
 				sorter = "case_sensitive",
 			},
 			view = {
-				width = 29,	
+				width = 29,
 	    	},
 			renderer = {
 				group_empty = true,
@@ -25,5 +25,13 @@ return {
 				dotfiles = true,
 			},
 		})
+
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+
 	end,
 }
